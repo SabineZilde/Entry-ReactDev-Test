076e9e130c8a1f1ce061a0f5e1df7a7d49c1ed43
SabineZilde <sabine.zilde@protonmail.ch>
2022-01-24 23:07:21 +0200

    style: navbar styling

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavBarButton } from './Header.styles';
+
+class Header extends React.Component {
+    render() {
+        return <NavBarButton>WOMEN</NavBarButton>;
+    }
+}
+
+export default Header;
---
+++ src/Components/Header/Header.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const NavBarButton = styled.button`
+    border: none;
+    border-bottom: solid 1px black;
+    background-color: white;
+    padding: 28px 10px 32px;
+    margin-left: 100px;
+`
