ca3260d878913b1573885b4f9da8e1dedf2a8eec
SabineZilde <sabine.zilde@protonmail.ch>
2022-01-25 11:22:31 +0200

    style: navbar button hover & focus

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { NavBarButton } from './Header.styles';
+import React from "react";
+import { NavBarButton } from "./Header.styles";
 
 class Header extends React.Component {
-    render() {
-        return <NavBarButton>WOMEN</NavBarButton>;
-    }
+  render() {
+    return (
+      <div>
+        <NavBarButton fontWeight='600'>WOMEN</NavBarButton>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/Components/Header/Header.styles.js
+++ src/Components/Header/Header.styles.js
@@ -2,8 +2,19 @@ import styled from 'styled-components';
 
 export const NavBarButton = styled.button`
     border: none;
-    border-bottom: solid 1px black;
     background-color: white;
-    padding: 28px 10px 32px;
+    padding: 28px 16px 32px;
     margin-left: 100px;
+    font-family: 'Raleway';
+    font-size: 16px;
+    font-weight: ${(props) => props.fontWeight};
+
+    &:hover {
+        color: #5ECE7B;
+    }
+
+    &:focus {
+        border-bottom: solid 2px #5ECE7B;
+        color: #5ECE7B;
+    }
 `
