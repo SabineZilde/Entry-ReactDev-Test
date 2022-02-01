a098dce34f4ece4e09b9b1c76b5f808b6035da40
SabineZilde <sabine.zilde@protonmail.ch>
2022-02-01 20:25:22 +0200

    style: cart layout

--- src/Views/Cart/Cart.js
+++ src/Views/Cart/Cart.js
@@ -1,9 +1,52 @@
 import React from "react";
+import {
+  CartContainer,
+  ProductContainer,
+  ProductDescription,
+  CountAndImg,
+  Column,
+  ProductImage,
+} from "./Cart.style";
+import { FontRaleway } from "../../Components/Fonts/Fonts.style";
+import {
+  AttributeButton,
+  QuantityButton,
+} from "../../Components/Buttons/Buttons.style";
+import product from "../../Assets/Product.png";
 
 class Cart extends React.Component {
   render() {
     return (
-        <div>This is cart page</div>
+      <CartContainer>
+        <FontRaleway fontSize="32px" fontWeight="700" margin="0 0 59px 0">
+          CART
+        </FontRaleway>
+        <ProductContainer>
+          <ProductDescription>
+            <FontRaleway fontSize="30px" fontWeight="600">
+              Apollo
+            </FontRaleway>
+            <FontRaleway fontSize="30px">Running Short</FontRaleway>
+            <FontRaleway fontSize="24px" fontWeight="700">
+              $50.00
+            </FontRaleway>
+            <div>
+              <AttributeButton margin="0 10px 0 0">XS</AttributeButton>
+              <AttributeButton margin="0 10px 0 0">S</AttributeButton>
+              <AttributeButton margin="0 10px 0 0">M</AttributeButton>
+              <AttributeButton margin="0 10px 0 0">L</AttributeButton>
+            </div>
+          </ProductDescription>
+          <CountAndImg>
+            <Column>
+              <QuantityButton>+</QuantityButton>
+              <div>1</div>
+              <QuantityButton>-</QuantityButton>
+            </Column>
+            <ProductImage backgroundImage={product} />
+          </CountAndImg>
+        </ProductContainer>
+      </CartContainer>
     );
   }
 }
---
+++ src/Views/Cart/Cart.style.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const CartContainer = styled.div`
+    margin: 80px 242px 54px 100px;
+`
+
+export const ProductContainer = styled.div`
+    margin-top: 20px;
+    border-top: 1px solid #E5E5E5;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const ProductDescription = styled.div`
+    height: 215px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+
+export const CountAndImg = styled.div`
+    display: flex;
+    margin: 21px 0;
+`
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 12px;
+`
+
+export const ProductImage = styled.div`
+    height: 185px;
+    width: 141px;
+    background-image: ${(props) =>
+        `url(${props.backgroundImage})`
+        };
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
