f8cd2793fc5524af1966403a0c0f8636a2382332
SabineZilde <sabine.zilde@protonmail.ch>
2022-03-22 10:16:08 +0200

    fix: cart img code refactoring
    Moving conditional statements into separate methods.

--- src/Components/CartProduct/CartProduct.js
+++ src/Components/CartProduct/CartProduct.js
@@ -24,6 +24,50 @@ class Cart extends React.Component {
     imgNumber: 0,
   };
 
+  setBgImg = (id, gallery) => {
+    if (this.state.itemId === id) {
+      return gallery[this.state.imgNumber];
+    } else {
+      return gallery[0];
+    }
+  };
+
+  handleLeftArrow = (id, length) => {
+    const { imgNumber } = this.state;
+    this.setState({
+      itemId: id,
+    });
+    if (length === 1) {
+      this.setState({
+        imgNumber: 0,
+      });
+    } else if (imgNumber === 0) {
+      this.setState({
+        imgNumber: length - 1,
+      });
+    } else {
+      this.setState({
+        imgNumber: imgNumber - 1,
+      });
+    }
+  };
+
+  handleRightArrow = (id, length) => {
+    const { imgNumber } = this.state;
+    this.setState({
+      itemId: id,
+    });
+    if (imgNumber < length - 1) {
+      this.setState({
+        imgNumber: imgNumber + 1,
+      });
+    } else {
+      this.setState({
+        imgNumber: 0,
+      });
+    }
+  };
+
   render() {
     const { productsInCart, contextCurrency, updateProductCount, showAlert } =
       this.context;
@@ -92,50 +136,20 @@ class Cart extends React.Component {
                 <ImgContainer>
                   <ArrowButton
                     left
-                    onClick={() => {
-                      this.setState({
-                        itemId: item.id,
-                      });
-                      if (item.gallery.length === 1) {
-                        this.setState({
-                          imgNumber: 0,
-                        });
-                      } else if (this.state.imgNumber === 0) {
-                        this.setState({
-                          imgNumber: item.gallery.length - 1,
-                        });
-                      } else {
-                        this.setState({
-                          imgNumber: this.state.imgNumber - 1,
-                        });
-                      }
-                    }}
+                    onClick={() =>
+                      this.handleLeftArrow(item.id, item.gallery.length)
+                    }
                   >
                     &lt;
                   </ArrowButton>
                   <ProductImage
                     key={item.id + item.gallery[this.state.imgNumber]}
-                    backgroundImage={
-                      this.state.itemId === item.id
-                        ? item.gallery[this.state.imgNumber]
-                        : item.gallery[0]
-                    }
+                    backgroundImage={this.setBgImg(item.id, item.gallery)}
                   />
                   <ArrowButton
-                    onClick={() => {
-                      this.setState({
-                        itemId: item.id,
-                      });
-                      if (this.state.imgNumber < item.gallery.length - 1) {
-                        this.setState({
-                          imgNumber: this.state.imgNumber + 1,
-                        });
-                      } else {
-                        this.setState({
-                          imgNumber: 0,
-                        });
-                      }
-                    }}
+                    onClick={() =>
+                      this.handleRightArrow(item.id, item.gallery.length)
+                    }
                   >
                     &gt;
                   </ArrowButton>
