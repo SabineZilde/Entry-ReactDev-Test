88f7c4abc83f3801023e3c7eb08ae1e77a0c8ec0
SabineZilde <sabine.zilde@protonmail.ch>
2022-04-05 13:46:20 +0300

    fix: code refactoring in Cart

--- src/Views/Cart/Cart.js
+++ src/Views/Cart/Cart.js
@@ -6,32 +6,39 @@ import MainContext from "../../Context/MainContext";
 
 class Cart extends React.Component {
   componentDidMount = () => {
-    if (this.context.productsInCart.length > 0) {
-      this.context.getTotal(this.context.contextCurrency);
+    const { productsInCart, contextCurrency, getTotal } = this.context;
+    if (productsInCart.length > 0) {
+      return getTotal(contextCurrency);
+    }
+  };
+
+  handleCartContent = () => {
+    const { productsInCart, contextCurrency, total } = this.context;
+    if (productsInCart.length === 0) {
+      return <FontRaleway>Your cart is empty</FontRaleway>;
+    } else {
+      return (
+        <>
+          <CartProduct />
+          <Total>
+            <FontRaleway fontSize="26px" fontWeight="700">
+              TOTAL: &nbsp;
+              {contextCurrency}
+              {total}
+            </FontRaleway>
+          </Total>
+        </>
+      )
     }
   };
 
   render() {
-    const { productsInCart, contextCurrency, total } = this.context;
     return (
       <CartContainer>
         <FontRaleway fontSize="32px" fontWeight="700" margin="0 0 59px 0">
           CART
         </FontRaleway>
-        {productsInCart.length === 0 ? (
-          <FontRaleway>Your cart is empty</FontRaleway>
-        ) : (
-          <>
-            <CartProduct />
-            <Total>
-              <FontRaleway fontSize="26px" fontWeight="700">
-                TOTAL: &nbsp;
-                {contextCurrency}
-                {total}
-              </FontRaleway>
-            </Total>
-          </>
-        )}
+        {this.handleCartContent()}
       </CartContainer>
     );
   }
