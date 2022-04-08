ad797506e4ec9c1512f00931b3be3bfb7fda4c74
SabineZilde <sabine.zilde@protonmail.ch>
2022-04-08 15:26:59 +0300

    fix: code refactoring in Attributes component

--- src/Components/ProductPage/Attributes.js
+++ src/Components/ProductPage/Attributes.js
@@ -4,50 +4,49 @@ import { FontRoboto } from "../../Components/Fonts.style";
 import MainContext from "../../Context/MainContext";
 
 class Attributes extends React.Component {
-  render() {
+  handleAttributes = () => {
     const { saveAttributes } = this.context;
-    return (
-      <div>
-        {this.props.product.attributes.map((attribute, id) => (
-          <div key={id}>
-            <FontRoboto
-              condensed
-              fontSize="18px"
-              fontWeight="700"
-              margin="10px 0"
-              capitalize
-            >
-              {attribute.name}:
-            </FontRoboto>
-            {attribute.items.map((item) => {
-              return (
-                <AttributesDiv key={item.id}>
-                  <Input
-                    type="radio"
-                    id={`${attribute.name} ${item.id}`}
-                    name={attribute.name}
-                    value={item.value}
-                    checkedColor={attribute.name !== "Color" && "#1D1F22"}
-                    checkedBorder={
-                      attribute.name === "Color" && "3px solid #A6A6A6"
-                    }
-                    onClick={() => {
-                      saveAttributes(attribute.name, item.value);
-                    }}
-                  />
-                  <Label
-                    htmlFor={`${attribute.name} ${item.id}`}
-                    bgColor={attribute.name === "Color" && item.value}
-                  >
-                    {attribute.name !== "Color" && item.value}
-                  </Label>
-                </AttributesDiv>
-              );
-            })}
-          </div>
-        ))}
+    return this.props.product.attributes.map((attribute, id) => (
+      <div key={id}>
+        <FontRoboto
+          condensed
+          fontSize="18px"
+          fontWeight="700"
+          margin="10px 0"
+          capitalize
+        >
+          {attribute.name}:
+        </FontRoboto>
+        {attribute.items.map((item) => {
+          return (
+            <AttributesDiv key={item.id}>
+              <Input
+                type="radio"
+                id={`${attribute.name} ${item.id}`}
+                name={attribute.name}
+                value={item.value}
+                checkedColor={attribute.name !== "Color" && "#1D1F22"}
+                checkedBorder={
+                  attribute.name === "Color" && "3px solid #A6A6A6"
+                }
+                onClick={() => {
+                  saveAttributes(attribute.name, item.value);
+                }}
+              />
+              <Label
+                htmlFor={`${attribute.name} ${item.id}`}
+                bgColor={attribute.name === "Color" && item.value}
+              >
+                {attribute.name !== "Color" && item.value}
+              </Label>
+            </AttributesDiv>
+          );
+        })}
       </div>
-    );
+    ))
+  }
+  render() {
+    return this.handleAttributes();
   }
 }
 
