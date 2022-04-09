80079f6dff0d23aec284ccca68e2ba250fbcff29
SabineZilde <sabine.zilde@protonmail.ch>
2022-04-09 19:54:03 +0300

    fix: code refactoring in productPage

--- src/Views/ProductPage/ProductPage.js
+++ src/Views/ProductPage/ProductPage.js
@@ -5,7 +5,6 @@ import {
   DescriptionRow,
 } from "./ProductPage.style";
 import { FontRoboto } from "../../Components/CommonStyles/Fonts.style";
-import { Query } from "@apollo/client/react/components";
 import { LOAD_PRODUCT } from "../../GraphQL/Queries";
 import { Loader } from "../../Components/CommonStyles/Loader.style";
 import Price from "../../Components/CommonComponents/Price";
@@ -14,49 +13,59 @@ import AddToCart from "../../Components/ProductPage/AddToCart";
 import Gallery from "../../Components/ProductPage/Gallery";
 import ProductName from "../../Components/CommonComponents/ProductName";
 import parse from "html-react-parser";
+import { client } from '../..';
 
 class ProductPage extends React.Component {
   state = {
-    id: '',
+    loading: true,
+    product: {}
   };
 
-  componentDidMount() {
-    let id = this.props.match.params.id;
-    this.setState({
-      id: id,
+  componentDidMount = async () => {
+    const response = await client.query({
+      query: LOAD_PRODUCT,
+      variables: {
+        id: this.props.match.params.id
+      }
     });
-  }
+    this.setState({
+      loading: response.loading,
+      product: response.data.product
+    });
+  };
+
+  handlePageContent = () => {
+    const { product, loading } = this.state;
+    if (loading) return <Loader />;
+    return (
+      <>
+        <Gallery gallery={product.gallery} />
+        <ProductDetailColumn>
+          <div>
+            <ProductName product={product} />
+          </div>
+          <div>
+            <Attributes product={product} />
+          </div>
+          <div>
+            <Price item={product} size='large' page='productPage' />
+          </div>
+          <AddToCart product={product} />
+          <DescriptionRow>
+            <FontRoboto>{parse(product.description)}</FontRoboto>
+          </DescriptionRow>
+        </ProductDetailColumn>
+      </>
+    );
+  };
 
   render() {
     return (
-      <Query query={LOAD_PRODUCT} variables={{ id: this.state.id }}>
-        {({ loading, data }) => {
-          if (loading) return <Loader />;
-          const { product } = data;
-          return (
-            <Row>
-              <Gallery gallery={product.gallery} />
-              <ProductDetailColumn>
-                <div>
-                  <ProductName product={product} />
-                </div>
-                <div>
-                  <Attributes product={product} />
-                </div>
-                <div>
-                  <Price item={product} size='large' page='productPage' />
-                </div>
-                <AddToCart product={product} />
-                <DescriptionRow>
-                  <FontRoboto>{parse(product.description)}</FontRoboto>
-                </DescriptionRow>
-              </ProductDetailColumn>
-            </Row>
-          );
-        }}
-      </Query>
+      <Row>
+        {this.handlePageContent()}
+      </Row>
     );
-  }
-}
+  };
+};
 
 export default ProductPage;
